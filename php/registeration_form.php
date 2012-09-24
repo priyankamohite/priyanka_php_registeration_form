48f80bbcd006fba7a2d7dd5a5d118f9f6d2820c2
webonise <webonise@webonise-desktop.(none)>
2012-09-24 14:36:17 +0530

    data inserted into table through form

--- php/registeration_form.php
+++ php/registeration_form.php
@@ -9,10 +9,28 @@ else
     echo "connected to the database";
 }
 
-// some code
+$firstNname=$_POST["Fname"];
+$lastName=$_POST["Lname"];
+$age=$_POST["age"];
+$gender=$_POST["gender"];
+$emailId=$_POST["email_id"];
+$address=$_POST["address"];
+$country=$_POST["country"];
+$state=$_POST["state"];
+$city=$_POST["city"];
+$pincode=$_POST["pincode"];
+
+mysql_select_db("registeration");
+$query=" insert into user_info(first_name,last_name,age,gender,email_id,address,country,state,city,pincode) values ('$firstNname','$lastName','$age','$gender','$emailId','$address','$country','$state','$city','$pincode')";
+
+$result=mysql_query($query);
+if($result)
+{
+  echo "data inserted";
+}
 
 mysql_close($con);
-echo "<br/>connection closed";
+echo "connection closed";
 ?>
 
 
